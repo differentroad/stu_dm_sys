9de716a75287438531d6f35dc2d0d3614e3c99de
differentroad <152857279+differentroad@users.noreply.github.com>
2025-07-13 16:41:45 +0800

    Delete Dormitory_business directory

--- Dormitory_business/src/main/java/com/example/springboot/common/UID.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.common;
-
-import java.util.UUID;
-
-public class UID {
-    public String produceUID() {
-        return UUID.randomUUID().toString().replaceAll("-", "");
-    }
-}
